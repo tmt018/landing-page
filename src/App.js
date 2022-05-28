dcf17a1b1bade7aa9cbb0d909e292789e9b75206
tmt018 <valentontimothy@gmail.com>
2022-05-28 13:32:32 +0800

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,225 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+    <div className="container">
+      <a className="navbar-brand js-scroll-trigger" href="#page-top">My Studio</a>
+      <button className="navbar-toggler navbar-dark navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i className="fa fa-bars"></i>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarResponsive">
+        <ul className="navbar-nav text-uppercase ml-auto">
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#services">Services</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#team">Team</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  
+  <header className="masthead">
+    <div className="container">
+      <div className="intro-text">
+        <div className="intro-lead-in">Hello and welcome to our studio!</div>
+        <div className="intro-heading text-uppercase">It's exciting to meet you</div>
+        <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
+      </div>
+    </div>
+  </header>
+
+  
+  <section className="page-section" id="services">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 text-center">
+          <h2 className="section-heading text-uppercase">Our Services</h2>
+          <h3 className="section-subheading text-light">Lorem ipsum dolor sit amet consectetur.</h3>
+        </div>
+      </div>
+      <div className="row text-center">
+        <div className="col-md-4">
+          <span className="fa-stack fa-4x">
+            <i className="fa fa-circle fa-stack-2x text-primary"></i>
+            <i className="fa fa-shopping-cart fa-stack-1x fa-inverse"></i>
+          </span>
+          <h4 className="service-heading">E-Commerce</h4>
+          <p className="text-muted">We offer the use of social media to create digital content and to advertise a product and build their business via all of the following digital platforms.</p>
+        </div>
+        <div className="col-md-4">
+          <span className="fa-stack fa-4x">
+            <i className="fa fa-circle fa-stack-2x text-primary"></i>
+            <i className="fa fa-laptop fa-stack-1x fa-inverse"></i>
+          </span>
+          <h4 className="service-heading">Responsive Design</h4>
+          <p className="text-muted">We create a design that focuses on creating a flexible website with content and design components that alter as the user moves around the screen.</p>
+        </div>
+        <div className="col-md-4">
+          <span className="fa-stack fa-4x">
+            <i className="fa fa-circle fa-stack-2x text-primary"></i>
+            <i className="fa fa-lock fa-stack-1x fa-inverse"></i>
+          </span>
+          <h4 className="service-heading">Web Security</h4>
+          <p className="text-muted">We aim to ensure that all information and knowledge required to support the security of a company's network and information systems is available safely and securely.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section className="bg-secondary page-section" id="team">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 text-center">
+          <h1 className="section-heading text-uppercase">Our Awesome Team</h1> 
+          <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-4">
+          <div className="team-member">
+            <h3>Daniel Lopez</h3>
+            <p className="text-dark">Creative Director</p>
+            <ul className="list-inline social-buttons">
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-twitter"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-facebook-f"></i>
+                </a>
+              </li>
+              </ul>
+          </div>
+        </div>
+        <div className="col-sm-4">
+          <div className="team-member">
+            <h3>Cherry Lefebre</h3>
+            <p className="text-dark">Marketing Specialist</p>
+            <ul className="list-inline social-buttons">
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-twitter"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-facebook-f"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-sm-4">
+          <div className="team-member">
+            <h3>Tracey Jenkins</h3>
+            <p className="text-dark">Project Manager</p>
+            <ul className="list-inline social-buttons">
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-twitter"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-facebook-f"></i>
+                </a>
+              </li>
+              </ul>
+          </div>
+        </div>
+      </div>
+      </div>
+  </section>
+
+  <section className="page-section" id="contact">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 text-center">
+          <h2 className="section-heading text-uppercase">Contact Us</h2>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-12">
+          <form id="contactForm" name="sentMessage" novalidate="novalidate">
+            <div className="row">
+              <div className="col-md-6">
+                <div className="form-group">
+                  <input className="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name."/>
+                  <p className="help-block text-danger"></p>
+                </div>
+                <div className="form-group">
+                  <input className="form-control" id="email" type="email" placeholder="Your Email *" required="required" data-validation-required-message="Please enter your email address."/>
+                  <p className="help-block text-danger"></p>
+                </div>
+                <div className="form-group">
+                  <input className="form-control" id="phone" type="tel" placeholder="Your Phone *" required="required" data-validation-required-message="Please enter your phone number."/>
+                  <p className="help-block text-danger"></p>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="form-group">
+                  <textarea className="form-control" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
+                  <p className="help-block text-danger"></p>
+                </div>
+              </div>
+              <div className="clearfix"></div>
+              <div className="col-lg-12 text-center">
+                <div id="success"></div>
+                <button id="sendMessageButton" className="btn btn-primary btn-xl text-uppercase" type="submit">Send Message</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+  <footer className="footer">
+    <div className="container">
+      <div className="row align-items-center">
+        <div className="col-md-4">
+          <span className="copyright">Copyright &copy; My Studio 2022</span>
+        </div>
+        <div className="col-md-4">
+          <ul className="list-inline social-buttons">
+          <li className="list-inline-item">
+              <a href="#something">
+                <i className="fa fa-facebook-f"></i>
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#something">
+                <i className="fa fa-twitter"></i>
+              </a>
+            </li>
+            </ul>
+        </div>
+        <div className="col-md-4">
+          <ul className="list-inline quicklinks">
+            <li className="list-inline-item">
+              <a href="#something">Privacy Policy</a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#something">Terms of Use</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
     </div>
   );
 }
